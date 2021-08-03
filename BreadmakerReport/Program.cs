eb6d0cdd0958132b037b45d293c42563c4e00b1a
parthvyas18896 <pvyas1@confederationcollege.ca>
2021-08-03 00:25:34 -0400

    Completed Assignment 1 part -B

---
+++ BreadmakerReport/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using RatingAdjustment.Services;
+using BreadmakerReport.Models;
+
+namespace BreadmakerReport
+{
+    class Program
+    {
+        static string dbfile = @".\data\breadmakers.db";
+        static RatingAdjustmentService ratingAdjustmentService = new RatingAdjustmentService();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Bread World");
+            var BreadmakerDb = new BreadMakerSqliteContext(dbfile);
+            var BMList = BreadmakerDb.Breadmakers
+                 // TODO: add LINQ logic ...
+                 //       ...
+                 .Select(b => new {
+                     Reviews = b.Reviews.Count,
+                     Average = (Double)b.Reviews.Average(a => a.stars),
+                     Adjust = ratingAdjustmentService.Adjust((Double)b.Reviews.Average(a => a.stars), (Double)b.Reviews.Count),
+                     Desc = b.title
+                 })
+                 .ToList()
+          
+                 .OrderByDescending(r => r.Adjust)
+                .ToList();
+
+            Console.WriteLine("[#]  Reviews Average  Adjust    Description");
+            for (var j = 0; j < 3; j++)
+            {
+                var i = BMList[j];
+                Console.WriteLine($"[{j+1}]   {i.Reviews}    {Math.Round(i.Average, 2)}    {Math.Round(i.Adjust,2)}    {i.Desc}");
+                // TODO: add output
+                // Console.WriteLine( ... );
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RatingAdjustmentService/RatingAdjustmentService.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace RatingAdjustment.Services
+{
+    /** Service calculating a star rating accounting for the number of reviews
+     * 
+     */
+    public class RatingAdjustmentService
+    {
+        const double MAX_STARS = 5.0;  // Likert scale
+        const double Z = 1.96; // 95% confidence interval
+
+        double _q;
+        double _percent_positive;
+
+        /** Percentage of positive reviews
+         * 
+         * In this case, that means X of 5 ==> percent positive
+         * 
+         * Returns: [0, 1]
+         */
+        void SetPercentPositive(double stars)
+        {
+            // TODO: Implement this!
+            _percent_positive = (stars * 20) / 100;
+
+
+
+        }
+
+        /**
+         * Calculate "Q" given the formula in the problem statement
+         */
+        void SetQ(double number_of_ratings)
+        {
+            // TODO: Implement this!
+            double num = number_of_ratings;
+            double percentage = _percent_positive;
+            _q = Z * Math.Sqrt(((percentage * (1 - percentage)) + ((Z * Z) / (4 * num))) / num);
+
+
+
+        }
+
+        /** Adjusted lower bound
+         * 
+         * Lower bound of the confidence interval around the star rating.
+         * 
+         * Returns: a double, up to 5
+         */
+        public double Adjust(double stars, double number_of_ratings)
+        {
+            // TODO: Implement this!
+            SetPercentPositive(stars);
+            SetQ(number_of_ratings);
+            double number = number_of_ratings;
+            double percentage = _percent_positive;
+            double IB = ((percentage + ((Z * Z) / (2 * number)) - _q) / (1 + ((Z * Z) / number))) * MAX_STARS;
+            if (IB <= MAX_STARS)
+            {
+                return IB;
+            }
+            else
+            {
+                return stars;
+            }
+
+        }
+    }
+}
